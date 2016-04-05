59daaccec37f103111ef8e5892bfef0842555482
buxuku <buxuku@gmail.com>
2016-04-05 23:04:23 +0800

    DOM选择器 连缀操作

--- base.js
+++ base.js
@@ -1,11 +1,57 @@
-var Base = {
-	Id : function(id){
-		return document.getElementById(id);
-	},
-	Name : function(name){
-		return document.getElementsByName(name);
-	},
-	Tag : function(tag){
-		return document.getElementsByTagName(tag);
+// var Base = {
+// 	Id : function(id){
+// 		return document.getElementById(id);
+// 	},
+// 	Name : function(name){
+// 		return document.getElementsByName(name);
+// 	},
+// 	Tag : function(tag){
+// 		return document.getElementsByTagName(tag);
+// 	}
+// }
+function $(){
+	return new base();
+}
+function base(){ //js中区分大小写，变量名和函数名不能同名，所以不能用var base = base();
+	this.elements = [];
+	this.Id = function(id){
+		this.elements.push(document.getElementById(id));
+		return this;
+	};
+	this.Name = function(name){
+		var name = document.getElementsByName(name);
+		for (var i = 0; i <name.length; i++) {
+			this.elements.push(name[i]);
+		};
+		return this;
+	};
+	this.Tag = function(tag){
+		var tags = document.getElementsByTagName(tag);
+		for (var i = 0; i <tags.length; i++) {
+			this.elements.push(tags[i]);
+		};
+		return this;
+	};
+}
+
+base.prototype.css = function(attr,value){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].style[attr] = value;
 	}
+	
+	return this;
+}
+
+base.prototype.html = function (str){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].innerHTML = str;
+	}
+	return this;
+}
+
+base.prototype.click = function (fn){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].onclick = fn;//xhtml中事件监听函数全部用小写，所以不能写成onClick;
+	}
+	return this;
 }
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 window.onload  = function(){
-	console.log(Base.Id('i').innerHTML);
-	var name = Base.Name('myname');
-	console.log(name.length);
-	for(i=0;i<name.length;i++){
-		console.log(name[i].value);
+	$().Tag('p').css("color","green").html("标题").click(function(){alert("ok");});
+	console.log($().Id('p').innerHTML);
+	console.log($().Id('i').elements);
+	console.log($().Id('i').elements);
+	//$().Id('i').css('color','red').css('background','red');
+	//alert($().Id('i').elements.length);
+	var name = $().Name('myname');
+	console.log(name.elements.length);
+	for(i=0;i<name.elements.length;i++){
+		console.log(name.elements[i].value);
 
 	}
-	var tag = Base.Tag('p');
-	for (var i = tag.length - 1; i >= 0; i--) {
-		console.log(tag[i].innerHTML);
+	var tag = $().Tag('p');
+	for (var i = tag.elements.length - 1; i >= 0; i--) {
+		console.log(tag.elements[i].innerHTML);
 	};
+	var reg = "d#My tme is LXD";
+	var regt = /#((?:[\w\u00c0-\uFFFF_-]|\\.)+)/;
+	if(regt.test(reg)){
+		console.log("find");
+	}
 }
